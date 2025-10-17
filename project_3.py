da38e714ee6ca90ac41d43489b19d97ab85381d3
Anthony Mitchell <mitchellanthony99@gmail.com>
2025-10-16 20:45:37 -0400

    add arg and standard for reps per color

--- project_3.py
+++ project_3.py
@@ -1,3 +1,4 @@
+import argparse
 import random
 
 
@@ -30,7 +31,8 @@ def select_test_colors(color):
 
     # TODO: write logic for returning an array of hex colors to display against the passed color value
 
-    return color_list  
+    color_list = random.shuffle(color_list)  # shuffle to avoid testing bias
+    return  color_list  
 
 
 class colorTest:
@@ -47,21 +49,29 @@ class colorTest:
         return f"{self.source},{hex(self.color)},{self.color_options},{self.chosen_color},{self.time}"  # csv separated string for logging
 
 
-test_colors = []
-test_per_color = 7  # TODO: Confirm this number for the correct amount of testing samples (turnn into runtime arg to modify as necessary?)
-
-for key, value in old_colors.items():
-    for i in range(test_per_color):
-        test_colors.append(colorTest(value, 'old'))
-
-for key, value in new_colors.items():
-    for i in range(test_per_color):
-        test_colors.append(colorTest(value, 'new'))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--reps", nargs='?', default=20,
+                        help="number of reps per color option. Default 20")
+    args = parser.parse_args()
 
 
-random.shuffle(test_colors)  # shuffle so we can avoid bias in testing order
+    test_colors = []
+    test_per_color = int(args.reps)
+    
 
-for test in test_colors:
-    print(test)
+    for key, value in old_colors.items():
+        for i in range(test_per_color):
+            test_colors.append(colorTest(value, 'old'))
+
+    for key, value in new_colors.items():
+        for i in range(test_per_color):
+            test_colors.append(colorTest(value, 'new'))
+
+
+    random.shuffle(test_colors)  # shuffle so we can avoid bias in testing order
+
+    for test in test_colors:
+        print(test)
 
 
